0e533403d534684407bb6f9c23631c353100d365
song <soliu@redhat.com>
2016-08-22 16:30:01 +0800

    add attach stress test cases

--- Test_Data/generate_test_data.py
+++ Test_Data/generate_test_data.py
@@ -23,12 +23,14 @@ class Accounts:
         self.db_username = "account_tool"
 
     def get_new_account(self):
-        for i in self.test_accounts:
-            res = requests.get(self.check_url,
-                               auth=(i, self.password),
-                               verify=False)
-            if res.status_code == 401 and "The system is unable to complete the requested transaction" in res.content:
-                return i
+        while 1:
+            account = random.sample(self.test_accounts, 1)[0]
+            res = requests.get(self.check_url, auth=(account, self.password), verify=False)
+            print "#"*20
+            print res.status_code
+            print res.content, type(res.content)
+            if res.status_code == 401 and "Invalid username or password" in res.content:
+                return account
 
     def get_sku(self, number):
         db = MySQLdb.connect(host=self.db_host, user=self.db_username, passwd=self.password, db="sku_db")
@@ -38,26 +40,43 @@ class Accounts:
         skus_list = [i[0] for i in d]
         return random.sample(skus_list, number)
 
+
+# Get different account for different line.
+def get_different_account(account1):
+    different_code = 0
+    while 1:
+        account2 = Accounts().get_new_account()
+        if account2 == account1:
+            Accounts().get_new_account(account1)
+        else:
+            different_code = 1
+        if different_code == 1:
+            return account2
+
+
 username_line1 = Accounts().get_new_account()
 skus_line1 = Accounts().get_sku(1)
-username_line2 = Accounts().get_new_account()
+username_line2 = get_different_account(username_line1)
 skus_line2 = Accounts().get_sku(10)
-username_line3 = Accounts().get_new_account()
+username_line3 = get_different_account(username_line2)
 skus_line3 = Accounts().get_sku(100)
-username_line4 = Accounts().get_new_account()
+username_line4 = get_different_account(username_line3)
 skus_line4 = Accounts().get_sku(1000)
-username_line5 = Accounts().get_new_account()
+username_line5 = get_different_account(username_line4)
 skus_line5 = Accounts().get_sku(10000)
-info_str = """
 
-"""
+info_str = """{0},,{1}
+{2},,{3}
+{4},,{5}
+{6},,{7}
+{8},,{9}
+""".format(username_line1, skus_line1, username_line2, skus_line2, username_line3, skus_line3, username_line4, skus_line4, username_line5, skus_line5)
+info_str = info_str.replace("\'", "\"")
 
-data_path = "/home/liusong/code/Ethel_Performance_Test/Test_Data/test_stress_account_attach.csv"
+data_path = "/home/workspace/Ethel_performance_test2/test_stress_account_attach.csv"
 with open(data_path, "a+") as f:
-    f.write()
+    f.write(info_str)
 
-
-print username, skus
 
 
 
